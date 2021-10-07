e8fe860336425e9ca4466856207f73bae8647421
Morley, Jonathan <jmorley@cvent.com>
2021-10-07 13:35:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+<h1 align="center">Welcome to aws-product-list 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000" />
+  <a href="#" target="_blank">
+    <img alt="License: ISC" src="https://img.shields.io/badge/License-ISC-yellow.svg" />
+  </a>
+</p>
+
+> List all AWS products, along with metadata
+
+## Install
+
+```sh
+asdf install
+npm install
+```
+
+## Usage
+
+```sh
+node .
+```
+
+## Author
+
+👤 **Jonathan Morley <morley.jonathan@gmail.com>**
+
+* Github: [@jonathanmorley](https://github.com/jonathanmorley)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ index.js
@@ -0,0 +1,30 @@
+import got from 'got';
+import { JSDOM } from 'jsdom';
+import groupBy from 'lodash.groupby';
+
+const baseUrl = new URL('https://aws.amazon.com/products');
+
+const dom = await got(baseUrl).then(({ body }) => new JSDOM(body));
+const productNodes = [...dom.window.document.querySelectorAll(".lb-content-item")];
+
+const products = productNodes
+  .map(productNode => {
+    return {
+      name: productNode.querySelector('span').textContent,
+      link: new URL(productNode.querySelector('a').href, baseUrl).href,
+      summary: productNode.querySelector('cite').textContent,
+      category: productNode.parentNode.parentNode.querySelector('span').textContent
+    };
+  })
+  .sort((a, b) => a.name.localeCompare(b.name, undefined, { sensitivity: 'base' }));
+
+const grouped = Object.entries(groupBy(products, product => product.name)).map(([k, vs]) => {
+  return {
+    name: k,
+    link: vs[0].link,
+    summary: vs[0].summary,
+    categories: vs.map(v => v.category)
+  };
+});
+
+console.dir(grouped, { maxArrayLength: null });
