4f2fcc6abdc2137be88f4b1585bf413193122fc2
Morley, Jonathan <jmorley@cvent.com>
2022-10-13 11:11:44 -0400

    remove html from the summaries

--- scripts/build-docs.mjs
+++ scripts/build-docs.mjs
@@ -1,5 +1,13 @@
 import fs from "node:fs";
 
+import he from "he";
+
+function handleHtml(html) {
+  var elem = document.createElement("textarea");
+  elem.innerHTML = html;
+  return elem.value;
+}
+
 const productsUrl = new URL(
   "https://aws.amazon.com/api/dirs/items/search?item.directoryId=aws-products&item.locale=en_US&size=1000"
 );
@@ -9,9 +17,13 @@ const { items } = await fetch(productsUrl).then((res) => res.json());
 const products = items.map(({ item, tags }) => ({
   id: item.name,
   name: item.additionalFields.productName,
-  summary: item.additionalFields.productSummary,
+  summary: he
+    .decode(item.additionalFields.productSummary)
+    .replaceAll(/<\/?p>/g, "")
+    .trim(),
   category: item.additionalFields.productCategory,
-  type: tags.find(tag => tag.tagNamespaceId === 'aws-products#type').description
+  type: tags.find((tag) => tag.tagNamespaceId === "aws-products#type")
+    .description,
 }));
 
 await fs.promises.mkdir("docs", { recursive: true });
